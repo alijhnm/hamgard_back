5b9a1bd3e61fb05f4f64e10eab359e4f22eaaf9f
alijhnm <ajahanmm@gmail.com>
2019-06-12 14:06:29 +0430

    modify settings.py to read databases and local hosts from local_settings.py

--- Hamgard/settings.py
+++ Hamgard/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from .local_settings import DATABASES_LOCAL, ALLOWED_HOSTS_LOCAL
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'ib9ya0rra!@gvb8cwzuwu6$iptb8!s^)9g!jj&en1xii#@mtf6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] + ALLOWED_HOSTS_LOCAL
 
 
 # Application definition
@@ -70,18 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Hamgard.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -99,6 +87,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+DATABASES = DATABASES_LOCAL
 
 AUTH_USER_MODEL = 'account.User'
 
