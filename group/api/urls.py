c119936c9f20491ed4995f6fb0c2098fd7c7d075
alijhnm <ajahanmm@gmail.com>
2019-06-14 16:43:08 +0430

    Add retrieve group api.

--- group/api/urls.py
+++ group/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from .create_group import create_group
+from.retrieve_group import group_detail, group_list
 
 urlpatterns = [
     path('creategroup/', create_group),
+    path('groups/', group_list),
+    path('group/', group_detail)
 ]
