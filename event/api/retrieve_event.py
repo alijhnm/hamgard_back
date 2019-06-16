80a007fff33b3415cc7fbf23e5fe6af0803b3af4
alijhnm <ajahanmm@gmail.com>
2019-06-16 21:47:16 +0430

    Add event retrievement api.

---
+++ event/api/retrieve_event.py
@@ -0,0 +1,43 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from .get_vendor import get_vendor
+from event.models import Event
+
+
+@csrf_exempt
+@get_vendor
+@require_http_methods(["GET"])
+def events_list(request, vendor):
+    events = Event.objects.filter(vendor=vendor)
+    return JsonResponse({"events": [serialize_event(event) for event in events]})
+
+
+@csrf_exempt
+@get_vendor
+@require_http_methods(["GET"])
+def event_detail(request, vendor):
+    data = json.loads(request.body)
+    event_id = data.get("event_id")
+    event = Event.objects.filter(id=event_id).first()
+    if not event:
+        return JsonResponse({"message": "Event not found"}, status=404)
+
+    if event.vendor != vendor:
+        return JsonResponse({"message": "Not vendor of this event."}, status=403)
+    
+    return JsonResponse(serialize_event(event))
+
+
+def serialize_event(event):
+    serialized = dict()
+    serialized["id"] = event.id
+    serialized["vendor"] = event.vendor.user.username
+    serialized["title"] = event.title
+    serialized["category"] = event.category.title
+    serialized["summary"] = event.summary
+    serialized["price"] = event.price
+    serialized["discount"] = event.discount
+    serialized["address"] = event.address.address_text
+    return serialized
--- event/urls.py
+++ event/urls.py
@@ -1,7 +1,10 @@
 from .api.create_event import create_event
+from .api.retrieve_event import *
 from django.urls import path
 
 
 urlpatterns = [
-    path('createevent/', create_event)
+    path('createevent/', create_event),
+    path('events/', events_list),
+    path('event/', event_detail)
 ]
