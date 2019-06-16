f4feca3c142273ff594ad5e3951abf1cda2d99f4
masoud <mmh_1377@yahoo.com>
2019-06-16 18:36:59 +0430

    Add logout of "service provider"

--- account/api 2/service provider_logout.py
+++ account/api 2/service provider_logout.py
@@ -1,14 +1,17 @@
+import json
+from django.contrib.auth import authenticate
 from django.http import JsonResponse
+from .utils import get_user
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from account.api.get_user import get_user
 
 
-@csrf_exempt
 @get_user
+@csrf_exempt
 @require_http_methods(['POST'])
 def log_out(request, user):
-
-    user.logout_user()
-    return JsonResponse({"message": "Successfully logged out."}, status=200)
-
+    if user:
+        user.logout_user()
+        return JsonResponse({"message": "Logged out successfully "}, status=200)
+    else:
+        return JsonResponse({"message": "User not found !"}, status=404)
