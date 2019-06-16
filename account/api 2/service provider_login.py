b8f90be3a4570ca2b569d3f5ee612349c88da8e6
masoud <mmh_1377@yahoo.com>
2019-06-16 11:35:16 +0430

    Add login of "service provider"

--- account/api 2/service provider_login.py
+++ account/api 2/service provider_login.py
@@ -1,18 +1,16 @@
 import json
+from django.contrib.auth import authenticate
 from django.http import JsonResponse
 
-def log_in(request):
 
+def log_in(request):
     data = json.loads(request.body)
     username = data.get('username')
     password = data.get('password')
 
     if username is None or password is None:
-        return JsonResponse({"message": "invalid params give"}, status=400)
+        return JsonResponse({"message": "Invalid username or password !"}, status=400)
 
-
-
-
-
-
-
+    user = authenticate(username=username, password=password)
+    if not user:
+        return JsonResponse({"message": "Incorrect username or password !"}, status=404)
