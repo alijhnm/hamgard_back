73cfb0e28a376bfb93487878ba1c1afe561b88b8
masoud <mmh_1377@yahoo.com>
2019-06-25 10:10:57 +0430

    Fix service provider sign up.

--- account/api2/service_provider_signup.py
+++ account/api2/service_provider_signup.py
@@ -1,7 +1,6 @@
 import json
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
 from django.http.response import JsonResponse
-from django.db.utils import IntegrityError
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from account.models import Vendor
@@ -13,36 +12,23 @@ def sign_up(request):
     data = json.loads(request.body)
     username = data.get('username')
     password = data.get('password')
-    company_name = data.get("company_name")
-    first_name = data.get("first_name")
-    last_name = data.get("last_name")
-    service_provider_phone_number = data.get('service_provider_phone_number')
-    company_phone_number = data.get("company_phone_number")
-    email = data.get("email")
+    national_number = data.get("national_number")
+    business_licence = data.get("business_licence")
+    name = data.get("name")
 
-    if (username is None)\
-            or (password is None) \
-            or (company_name is None) \
-            or (first_name is None) \
-            or (last_name is None)\
-            or (service_provider_phone_number is None)\
-            or (company_phone_number is None)\
-            or (email is None):
-            return JsonResponse({"message": "invalid params give"}, status=400)
+    user = authenticate(usernaem=username, password=password)
 
-    user = Vendor(
+    if (national_number is None) \
+            or (name is None) \
+            or (business_licence is None):
+        return JsonResponse({"message": "invalid params give"}, status=400)
 
-                username=username,
-                password=make_password(password),
-                company_name=company_name,
-                first_name=first_name,
-                last_name=last_name,
-                service_provider_phone_number=service_provider_phone_number,
-                company_phone_number=company_phone_number,
-                email=email
+    vendor = Vendor(
+                user=user,
+                national_number=national_number,
+                business_licence=business_licence,
+                name=name
                 )
-    try:
-        user.save()
-    except IntegrityError:
-        return JsonResponse({'message': "Username already taken."}, status=400)
+    vendor.save()
+    return JsonResponse({"message": "Vendor is created successfully"})
 
--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 
 urlpatterns = [
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('api/v1/', include('account.api.urls'))
 =======
     path('api/', include('account.api 2.urls'))
 >>>>>>> Fix service provider login
+=======
+    path('api/', include('account.api2.urls'))
+>>>>>>> Fix service provider sign up.
 ]
