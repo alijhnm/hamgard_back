f5d82a53a4e17b988119a9fc565b947608feb7eb
alijhnm <ajahanmm@gmail.com>
2019-07-07 17:03:24 +0430

    Add poll app and Poll models and model requirements

--- Hamgard/settings.py
+++ Hamgard/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'group',
-    'event'
+    'event',
+    "poll"
 ]
 
 MIDDLEWARE = [
---
+++ poll/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.questions
+
+
+class PollChoice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name="choices")
+    text = models.CharField(max_length=200, blank=True, null=True)
+    choice_count = models.IntegerField(blank=True, default=0, null=True)
+
+    def __str__(self):
+        return self.text
+
+    def vote(self):
+        self.choice_count += 1
