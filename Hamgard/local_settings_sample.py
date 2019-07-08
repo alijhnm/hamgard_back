5ef2b08721b814d1e607fc57219aaf875fa25716
alijhnm <ajahanmm@gmail.com>
2019-07-08 23:30:02 +0430

    Add local_settings_sample

---
+++ Hamgard/local_settings_sample.py
@@ -0,0 +1,13 @@
+DATABASES_LOCAL = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        'USER': 'alijhnm',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+ALLOWED_HOSTS_LOCAL = ["*"]
+
