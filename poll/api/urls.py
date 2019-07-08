e84e3fb7c31395057060ac969f3ab6ad4772c1ed
hossein76rahimi <hossein76rahimi@gmail.com>
2019-07-09 03:07:44 +0430

    Add retrieve poll data

--- poll/api/urls.py
+++ poll/api/urls.py
@@ -1,9 +1,12 @@
 from poll.api.create_poll import create_poll
 from poll.api.retrieve_poll import get_group_polls, get_poll
+from poll.api.retrieve_poll_data import retrieve_poll_data
 from django.urls import path
 
 urlpatterns = [
     path('retrievepolls/', get_group_polls),
     path('createpoll/', create_poll),
-    path('retrievepoll/', get_poll)
+    path('retrievepoll/', get_poll),
+    path('retrievepolldata/', retrieve_poll_data),
+
 ]
