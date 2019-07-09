987f2fb69cf8de8766b6645ccd84a16b7c6a146c
alijhnm <ajahanmm@gmail.com>
2019-07-09 21:35:27 +0430

    Add get_places api

---
+++ event/api/get_place.py
@@ -0,0 +1,28 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from account.api.get_user import get_user
+from event.models import Place
+
+
+@csrf_exempt
+@get_user
+@require_http_methods(["GET"])
+def get_places(request, user):
+    places = Place.objects.all()
+    data =serialize_places(places)
+    return JsonResponse(data, status=200)
+
+
+def serialize_places(queryset):
+    data = list()
+    for place in queryset:
+        serialized = dict()
+        serialized["id"] = place.pk
+        serialized["name"] = place.name_en
+        serialized["address"] = place.address.address_text
+        serialized["city"] = place.city
+        serialized["tags"] = [tag.name for tag in place.tags.all()]
+        serialized["category"] = place.category.name_en
+        data.append(serialized)
+    return data
--- event/api/urls.py
+++ event/api/urls.py
@@ -1,9 +1,11 @@
 from .retrieve_event import *
 from .create_event import create_event
+from .get_place import get_places
 from django.urls import path
 
 urlpatterns = [
     path('createevent/', create_event),
     path('events/', events_list),
-    path('event/', event_detail)
+    path('event/', event_detail),
+    path('places/', get_places)
 ]
