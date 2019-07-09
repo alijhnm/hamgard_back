845baf88f80dd9f0efd5aa5a8acd4eef41ce9bb2
hossein76rahimi <hossein76rahimi@gmail.com>
2019-07-10 02:10:23 +0430

    Add search apis

---
+++ event/api/search_event.py
@@ -0,0 +1,41 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from .get_vendor import get_vendor
+from event.models import Event
+from event.models import Place
+from account.api.get_user import get_user
+from django.db.models import Q
+# @csrf_exempt
+# @get_vendor
+# @require_http_methods(["GET"])
+# def events_list(request, vendor):
+#     events = Event.objects.filter(vendor=vendor)
+#     return JsonResponse({"events": [serialize_event(event) for event in events]})
+
+
+@csrf_exempt
+@get_user
+@require_http_methods(["GET"])
+def search_event(request, vendor):
+    q = request.GET.get("q")
+    event = Event.objects.filter(Q(title__icontains= q))
+    if not event:
+        return JsonResponse([], safe=False, status=200)
+    return JsonResponse(serialize_event(event), safe=False, status=200)
+
+def serialize_event(queryset):
+    data = list()
+    for event in queryset:
+        serialized = dict()
+        serialized["id"] = event.id
+        serialized["vendor"] = event.vendor.user.username
+        serialized["title"] = event.title
+        serialized["category"] = event.category.title
+        serialized["summary"] = event.summary
+        serialized["price"] = event.price
+        serialized["discount"] = event.discount
+        serialized["address"] = event.address.address_text
+        data.append(serialized)
+    return data
--- event/api/urls.py
+++ event/api/urls.py
@@ -1,11 +1,18 @@
-from .retrieve_event import *
+from django.urls import path
+
+from .search_event import search_event
+
 from .create_event import create_event
 from .get_place import get_places
-from django.urls import path
+from .retrieve_event import *
+from .search_place import search_place
 
 urlpatterns = [
     path('createevent/', create_event),
     path('events/', events_list),
     path('event/', event_detail),
-    path('places/', get_places)
+    path('places/', get_places),
+    path('places/search/', search_place),
+    path('events/search/', search_event)
+
 ]
