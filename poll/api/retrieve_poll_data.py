b4039a376d63c59c7546ed6239b659d275677e7d
alijhnm <ajahanmm@gmail.com>
2019-07-09 12:39:38 +0430

    git status

--- poll/api/retrieve_poll_data.py
+++ poll/api/retrieve_poll_data.py
@@ -1,26 +1,17 @@
-import json
-from random import random
-
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from account.api.get_user import get_user
-from account.models import Group
 from event.models import Event, Place
-from poll.models import *
 
 
 @csrf_exempt
 @get_user
 @require_http_methods(["GET"])
 def retrieve_poll_data(request, user):
-    group_id = request.GET.get('group_id', 1)
-    a = random.randint(0, Event.objects.all().count()-6)
-    e = Event.objects.all()[a:a+5]
-    p = Place.objects.all()[a:a+5]
+    e = Event.objects.all()
+    p = Place.objects.all()
     data = []
     for i in e:
         data.append({"id": i.id, 'type': 'event'})
